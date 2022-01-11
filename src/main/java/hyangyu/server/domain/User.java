ee4267bac3b9aee6f5bc4f336901e2967f017db1
RudinP <65944851+RudinP@users.noreply.github.com>
2022-01-11 20:51:57 +0900

    [feat]유저 도메인 추가(#3)
    User.java파일을 추가함.

---
+++ src/main/java/hyangyu/server/domain/User.java
@@ -0,0 +1,39 @@
+package hyangyu.server.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long user_id;
+	
+	@Column(nullable = false, length = 50)
+	private String email;
+	
+	@Column(nullable = false, length = 200)
+	private String password;
+	
+	@Column(nullable = false, length = 10)
+	private String nickname;
+	
+	@Column(length = 50)
+	private String sub;
+	
+	@Column(length = 100)
+	private String token;
+}
