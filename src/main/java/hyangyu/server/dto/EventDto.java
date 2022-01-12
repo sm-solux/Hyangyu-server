a2b3ecc6be3c22f6f115c95faff4dbbc7f9b6809
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-01-12 23:26:57 +0900

    [feat] eventDto 생성(#12)

--- src/main/java/hyangyu/server/dto/EventDto.java
+++ src/main/java/hyangyu/server/dto/EventDto.java
@@ -1,2 +1,29 @@
-package hyangyu.server.dto;public class EventDto {
+package hyangyu.server.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import java.sql.Date;
+import java.sql.Time;
+
+@Data
+public class EventDto {
+    private Long eventId;
+    private Date startDate;
+    private Date endDate;
+    private String title;
+    private int likey;
+    private int reviews;
+    private Time weekdayOpen;
+    private Time weekdayClose;
+    private Time weekendOpen;
+    private Time weekendClose;
+    private String location;
+    private String site;
+    private Date holiday;
+    private String content;
+    private String photo1;
+    private String photo2;
+    private String photo3;
+    private int price;
 }
