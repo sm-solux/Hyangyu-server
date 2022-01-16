363f8a59672868abdf3fc6ef4b082e1fb42d3484
RudinP <song_e@sookmyung.ac.kr>
2022-01-16 16:00:22 +0900

    [feat]jwt 로그인, 회원가입 구현(feat/#23)
    SecurityConfig 수정,
    JwtAccessDeniedHandler,JwtAuthenticationEntryPoint,JwtFilter,JwtSecurityConfig,TokenProvider
    추가
    #23

--- src/main/java/hyangyu/config/SecurityConfig.java
+++ src/main/java/hyangyu/config/SecurityConfig.java
@@ -1,26 +1,77 @@
 package hyangyu.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import hyangyu.jwt.JwtAccessDeniedHandler;
+import hyangyu.jwt.JwtAuthenticationEntryPoint;
+import hyangyu.jwt.JwtSecurityConfig;
+import hyangyu.jwt.TokenProvider;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
-	
+	  private final TokenProvider tokenProvider;
+	  private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	  private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+	  public SecurityConfig(
+	            TokenProvider tokenProvider,
+	            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+	            JwtAccessDeniedHandler jwtAccessDeniedHandler
+	    ) {
+	        this.tokenProvider = tokenProvider;
+	        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+	        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+	    }
+	  
+	    @Bean
+	    public PasswordEncoder passwordEncoder() {
+	        return new BCryptPasswordEncoder();
+	    }
 /*	@Override
 	public void configure(WebSecurity web) {
 		web
 					.ignoring()
 					.antMatchers(h2console안써서안해도되나?);
 	}*/
-	@Override
-	protected void configure(HttpSecurity http) throws Exception{
-		http
-					.authorizeRequests()
-					.antMatchers("/user/{id}").permitAll()
-					.anyRequest().authenticated();
-	}
+	    @Override
+	    protected void configure(HttpSecurity httpSecurity) throws Exception {
+	        httpSecurity
+	                // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
+	                .csrf().disable()
+
+	                .exceptionHandling()
+	                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+	                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+	                .and()
+	                .headers()
+	                .frameOptions()
+	                .sameOrigin()
+	                
+	                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+	                .and()
+	                .sessionManagement()
+	                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+	                .and()
+	                .authorizeRequests()
+	                .antMatchers("/user/{id}").permitAll()
+	                .antMatchers("/api/authenticate").permitAll()
+	                .antMatchers("/api/signup").permitAll()
+	                .anyRequest().authenticated()
+
+	                .and()
+	                .apply(new JwtSecurityConfig(tokenProvider));
+	    }
 	
 }
