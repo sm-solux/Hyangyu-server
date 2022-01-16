4b424db18a196e6c74a646608e0e681b32109fcf
RudinP <song_e@sookmyung.ac.kr>
2022-01-16 15:10:40 +0900

    [feat]도메인수정(feat/#23)
    User도메인 수정, Authority 도메인 추가,SpringSecurity 의존성 추가
    #23

--- src/main/java/hyangyu/server/domain/User.java
+++ src/main/java/hyangyu/server/domain/User.java
@@ -1,12 +1,18 @@
 package hyangyu.server.domain;
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotEmpty;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,7 +26,9 @@ import lombok.Getter;
 @Entity
 public class User {
 
+	@JsonIgnore
 	@Id
+	@Column(name = "user_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long userId;
 	
@@ -36,10 +44,11 @@ public class User {
 	@Column(length = 10)
 	private String nickname;
 	
-	@Column(length = 50)
-	private String sub;
-	
-	@NotNull
-	@Column( length = 100)
-	private String token;
+	@ManyToMany
+	@JoinTable(
+			name = "user_authority",
+			joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
+			inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
+	private Set<Authority> authorities;
+			
 }
