89d492dbd71b22ff3121faa976e4b88b257cf9de
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-01-25 00:51:14 +0900

    [feat] 내가 저장한 박람회 service 생성 (#31)

--- src/main/java/hyangyu/server/service/FavoriteFairService.java
+++ src/main/java/hyangyu/server/service/FavoriteFairService.java
@@ -1,2 +1,33 @@
-package hyangyu.server.service;public class FavoriteFairService {
+package hyangyu.server.service;
+
+import hyangyu.server.domain.*;
+import hyangyu.server.repository.FairRepository;
+import hyangyu.server.repository.FavoriteFairRepository;
+import hyangyu.server.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class FavoriteFairService {
+    private final FavoriteFairRepository favoriteFairRepository;
+    private final FairRepository fairRepository;
+    private final UserRepository userRepository;
+
+
+    @Transactional(readOnly = false)
+    public Optional<FavoriteFair> saveFavoriteFair(Long userId, Long fairId) {
+        Optional<User> user = userRepository.findByUserId(userId);
+        Optional<Fair> fair = fairRepository.findOne(fairId);
+        FavoriteFair favoriteFair = new FavoriteFair(user.get(), fair.get());
+        Optional<FavoriteFair> result = Optional.ofNullable(favoriteFairRepository.findOne(favoriteFair.getFavoriteFairId()));
+        if (result.isEmpty()) {
+            favoriteFairRepository.saveFavoriteFair(favoriteFair);
+        }
+        return result;
+    }
 }
