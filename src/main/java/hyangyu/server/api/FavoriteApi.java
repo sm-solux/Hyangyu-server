334d55c25228cf19b4282581cbe174119c63efd4
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-01-25 00:54:35 +0900

    [fix] 내가 저장한 박람회, 페스티벌 저장 api 추가 (#31)

--- src/main/java/hyangyu/server/api/FavoriteApi.java
+++ src/main/java/hyangyu/server/api/FavoriteApi.java
@@ -1,13 +1,10 @@
 package hyangyu.server.api;
 
-import hyangyu.server.domain.Display;
-import hyangyu.server.domain.FavoriteDisplay;
-import hyangyu.server.domain.User;
+import hyangyu.server.domain.*;
 import hyangyu.server.dto.ErrorDto;
+import hyangyu.server.dto.EventDto;
 import hyangyu.server.dto.SaveFavoriteResponseDto;
-import hyangyu.server.service.DisplayService;
-import hyangyu.server.service.FavoriteDisplayService;
-import hyangyu.server.service.UserService;
+import hyangyu.server.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -16,6 +13,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Date;
+import java.sql.Time;
 import java.util.Optional;
 
 @RestController
@@ -24,6 +24,12 @@ import java.util.Optional;
 public class FavoriteApi {
     private final DisplayService displayService;
     private final FavoriteDisplayService favoriteDisplayService;
+
+    private final FairService fairService;
+    private final FavoriteFairService favoriteFairService;
+
+    private final FestivalService festivalService;
+    private final FavoriteFestivalService favoriteFestivalService;
     private final UserService userService;
 
     @PostMapping("/display/{displayId}")
@@ -35,22 +41,84 @@ public class FavoriteApi {
         //전시 검색
         Optional<Display> display = displayService.findOne(displayId);
         if(display.isEmpty()) {
-            return new ResponseEntity(new ErrorDto(400, "잘못된 전시 번호입니다."), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity(new ErrorDto(404, "잘못된 전시 id입니다."), HttpStatus.BAD_REQUEST);
         }
 
         //사용자 검색
         Optional<User> user = userService.findUser(userId);
         if(user.isEmpty()) {
-            return new ResponseEntity(new ErrorDto(400, "유효하지 않은 사용자입니다."), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity(new ErrorDto(401, "유효하지 않은 사용자입니다."), HttpStatus.BAD_REQUEST);
         }
 
         //저장한 전시회 검색
         Optional<FavoriteDisplay> favoriteDisplay = favoriteDisplayService.saveFavoriteDisplay(userId, displayId);
         if(favoriteDisplay.isPresent()) {
-            return new ResponseEntity(new ErrorDto(400, "이미 저장한 전시회입니다."), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity(new ErrorDto(404, "이미 저장한 전시회입니다."), HttpStatus.BAD_REQUEST);
         }
 
         SaveFavoriteResponseDto saveFavoriteResponseDto = new SaveFavoriteResponseDto(200, "내가 저장한 전시회에 추가되었습니다.");
+        return new ResponseEntity<>(saveFavoriteResponseDto, httpHeaders, HttpStatus.OK);
+    }
+
+    @PostMapping("/fair/{fairId}")
+    public ResponseEntity saveFair(@PathVariable Long fairId) throws Exception {
+        Long userId = 1L;
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        EventDto eventDto = new EventDto(Date.valueOf("2021-01-24"), Date.valueOf("2021-01-28"), "테스트 박람회", 5, 0, Time.valueOf("09:00:00"), Time.valueOf("18:00:00"), Time.valueOf("09:00:00"), Time.valueOf("18:00:00"), "서울", "향유박람회", Date.valueOf("2021-01-26"), "세부내용", "", "", "", 0);
+        Fair fair1 = Fair.createFair(eventDto);
+        fairService.saveFair(fair1);
+
+        //박람회 검색
+        Optional<Fair> fair = fairService.findOne(fairId);
+        if(fair.isEmpty()) {
+            return new ResponseEntity(new ErrorDto(404, "잘못된 박람회 id입니다."), HttpStatus.BAD_REQUEST);
+        }
+
+        //사용자 검색
+        Optional<User> user = userService.findUser(userId);
+        if(user.isEmpty()) {
+            return new ResponseEntity(new ErrorDto(401, "유효하지 않은 사용자입니다."), HttpStatus.BAD_REQUEST);
+        }
+
+        //저장한 전시회 검색
+        Optional<FavoriteFair> favoriteFair = favoriteFairService.saveFavoriteFair(userId, fairId);
+        if(favoriteFair.isPresent()) {
+            return new ResponseEntity(new ErrorDto(404, "이미 저장한 박람회입니다."), HttpStatus.BAD_REQUEST);
+        }
+
+        SaveFavoriteResponseDto saveFavoriteResponseDto = new SaveFavoriteResponseDto(200, "내가 저장한 박람회에 추가되었습니다.");
+        return new ResponseEntity<>(saveFavoriteResponseDto, httpHeaders, HttpStatus.OK);
+    }
+
+    @PostMapping("/festival/{festivalId}")
+    public ResponseEntity saveFestival(@PathVariable Long festivalId) throws Exception {
+        Long userId = 1L;
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        EventDto eventDto = new EventDto(Date.valueOf("2021-01-24"), Date.valueOf("2021-01-28"), "테스트 페스티벌", 5, 0, Time.valueOf("09:00:00"), Time.valueOf("18:00:00"), Time.valueOf("09:00:00"), Time.valueOf("18:00:00"), "서울", "향유페스티벌", Date.valueOf("2021-01-26"), "세부내용", "", "", "", 0);
+        Festival f = Festival.createFestival(eventDto);
+        festivalService.saveFestival(f);
+
+        //박람회 검색
+        Optional<Festival> festival = festivalService.findOne(festivalId);
+        if(festival.isEmpty()) {
+            return new ResponseEntity(new ErrorDto(404, "잘못된 페스티벌 id입니다."), HttpStatus.BAD_REQUEST);
+        }
+
+        //사용자 검색
+        Optional<User> user = userService.findUser(userId);
+        if(user.isEmpty()) {
+            return new ResponseEntity(new ErrorDto(401, "유효하지 않은 사용자입니다."), HttpStatus.BAD_REQUEST);
+        }
+
+        //저장한 전시회 검색
+        Optional<FavoriteFestival> favoriteFestival = favoriteFestivalService.saveFavoriteFestival(userId, festivalId);
+        if(favoriteFestival.isPresent()) {
+            return new ResponseEntity(new ErrorDto(404, "이미 저장한 페스티벌입니다."), HttpStatus.BAD_REQUEST);
+        }
+
+        SaveFavoriteResponseDto saveFavoriteResponseDto = new SaveFavoriteResponseDto(200, "내가 저장한 페스티벌에 추가되었습니다.");
         return new ResponseEntity<>(saveFavoriteResponseDto, httpHeaders, HttpStatus.OK);
     }
 }
