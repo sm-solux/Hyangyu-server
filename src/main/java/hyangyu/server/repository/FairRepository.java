60957f171741befb5b6bb87e702befe52fd1d8b2
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-01-25 00:50:01 +0900

    [feat] 박람회 repository 생성 (#31)

--- src/main/java/hyangyu/server/repository/FairRepository.java
+++ src/main/java/hyangyu/server/repository/FairRepository.java
@@ -1,2 +1,22 @@
-package hyangyu.server.repository;public class FairRepository {
+package hyangyu.server.repository;
+
+import hyangyu.server.domain.Fair;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class FairRepository {
+    private final EntityManager em;
+
+    public void saveFair(Fair fair) {
+        em.persist(fair);
+    }
+
+    public Optional<Fair> findOne(Long fairId) {
+        return Optional.ofNullable(em.find(Fair.class, fairId));
+    }
 }
