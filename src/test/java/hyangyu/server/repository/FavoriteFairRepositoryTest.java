4a180e4be1e4e0365f7e078c036893afde83be7f
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-01-25 00:49:34 +0900

    [test] repository 테스트 (#31)

--- src/test/java/hyangyu/server/repository/FavoriteFairRepositoryTest.java
+++ src/test/java/hyangyu/server/repository/FavoriteFairRepositoryTest.java
@@ -1,2 +1,49 @@
-package hyangyu.server.repository;public class FavoriteFairRepositoryTest {
+package hyangyu.server.repository;
+
+import hyangyu.server.domain.*;
+import hyangyu.server.dto.EventDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.sql.Date;
+import java.sql.Time;
+
+@SpringBootTest
+@Transactional
+public class FavoriteFairRepositoryTest {
+    @Autowired
+    FavoriteFairRepository favoriteFairRepository;
+    @Autowired
+    FairRepository fairRepository;
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void saveFair() {
+
+        //given
+
+        //사용자 생성
+        User user = User.createUser("hyangyu@google.com", "abcd1234", "향유향유", null, "token", null);
+        em.persist(user);
+
+        //박람회 생성
+        EventDto eventDto = new EventDto(Date.valueOf("2021-01-24"), Date.valueOf("2021-01-28"), "테스트 전시2", 5, 0, Time.valueOf("09:00:00"), Time.valueOf("18:00:00"), Time.valueOf("09:00:00"), Time.valueOf("18:00:00"), "서울", "향유미술관", Date.valueOf("2021-01-26"), "세부내용", "", "", "", 0);
+        Fair fair = Fair.createFair(eventDto);
+        fairRepository.saveFair(fair);
+
+        //내가 저장한 박람회 생성
+        FavoriteFair favoriteFair = new FavoriteFair(user, fair);
+
+        //when
+        favoriteFairRepository.saveFavoriteFair(favoriteFair);
+
+        //then
+        Assertions.assertEquals(fair, fairRepository.findOne(fair.getFairId()).get());
+        Assertions.assertEquals(fair, favoriteFairRepository.findOne(favoriteFair.getFavoriteFairId()).getFair());
+    }
 }
