90a068e78a8556f7bd9be934d37d0beea0ac74ce
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-01-25 00:51:59 +0900

    [feat] 내가 저장한 페스티벌 service 생성 (#31)

--- src/main/java/hyangyu/server/service/FavoriteFestivalService.java
+++ src/main/java/hyangyu/server/service/FavoriteFestivalService.java
@@ -1,2 +1,35 @@
-package hyangyu.server.service;public class FavoriteFestivalService {
+package hyangyu.server.service;
+
+
+import hyangyu.server.domain.Festival;
+import hyangyu.server.domain.FavoriteFestival;
+import hyangyu.server.domain.Festival;
+import hyangyu.server.domain.User;
+import hyangyu.server.repository.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class FavoriteFestivalService {
+
+    private final FestivalRepository festivalRepository;
+    private final FavoriteFestivalRepository favoriteFestivalRepository;
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly = false)
+    public Optional<FavoriteFestival> saveFavoriteFestival(Long userId, Long festivalId) {
+        Optional<User> user = userRepository.findByUserId(userId);
+        Optional<Festival> festival = festivalRepository.findOne(festivalId);
+        FavoriteFestival favoriteFestival = new FavoriteFestival(user.get(), festival.get());
+        Optional<FavoriteFestival> result = Optional.ofNullable(favoriteFestivalRepository.findOne(favoriteFestival.getFavoriteFestivalId()));
+        if (result.isEmpty()) {
+            favoriteFestivalRepository.saveFavoriteFestival(favoriteFestival);
+        }
+        return result;
+    }
 }
