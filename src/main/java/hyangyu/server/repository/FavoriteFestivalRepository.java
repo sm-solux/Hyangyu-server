320fb072f8664266b4b2889b2e1a3811babd96d2
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-01-25 00:52:21 +0900

    [feat] 내가 저장한 페스티벌 repository 생성 (#31)

--- src/main/java/hyangyu/server/repository/FavoriteFestivalRepository.java
+++ src/main/java/hyangyu/server/repository/FavoriteFestivalRepository.java
@@ -1,2 +1,22 @@
-package hyangyu.server.repository;public class FavoriteFestivalRepository {
+package hyangyu.server.repository;
+
+import hyangyu.server.domain.FavoriteFestival;
+import hyangyu.server.domain.FavoriteFestivalId;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class FavoriteFestivalRepository {
+    private final EntityManager em;
+
+    public void saveFavoriteFestival(FavoriteFestival favoriteFestival) {
+        em.persist(favoriteFestival);
+    }
+
+    public FavoriteFestival findOne(FavoriteFestivalId favoriteFestivalId) {
+        return em.find(FavoriteFestival.class, favoriteFestivalId);
+    }
 }
