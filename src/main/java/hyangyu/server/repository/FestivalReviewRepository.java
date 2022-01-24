3dbdca1be7c14ea16bc3268d7f2a2827cc615f4d
yujin113 <claire9585@sookmyung.ac.kr>
2022-01-24 16:27:31 +0900

    [refactor] ReviewRepository에 리뷰 카운트 함수 추가(#24)

--- src/main/java/hyangyu/server/repository/FestivalReviewRepository.java
+++ src/main/java/hyangyu/server/repository/FestivalReviewRepository.java
@@ -2,6 +2,10 @@ package hyangyu.server.repository;
 
 import hyangyu.server.domain.FestivalReview;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FestivalReviewRepository extends JpaRepository<FestivalReview, Long> {
+
+    @Query("select count(r) from FestivalReview r where r.festival.festivalId=?1 and r.user.userId=?2")
+    public int getCount(Long festivalId, Long userId);
 }
