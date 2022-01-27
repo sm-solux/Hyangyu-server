c9a7bbd6fdf72d66f6317c2e384bb2f178a8c7f4
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-01-27 11:12:12 +0900

    [feat] MyPageResponseDto 작성 (#36)

--- src/main/java/hyangyu/server/dto/MyPageResponseDto.java
+++ src/main/java/hyangyu/server/dto/MyPageResponseDto.java
@@ -1,2 +1,24 @@
-package hyangyu.server.dto;public class MyPageResponseDto {
+package hyangyu.server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class MyPageResponseDto {
+
+    String photo;
+    String username;
+    List<EventDto> displays;
+    List<EventDto> fairs;
+    List<EventDto> festivals;
+    List<ReviewDto> reveiws;
+
+    /*
+
+    orderBy endDate
+     */
+
 }
