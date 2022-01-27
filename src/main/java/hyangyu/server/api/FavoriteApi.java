e025982ccb080e2f2e803ab919b28fb9e3d6bbd5
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-01-27 11:13:13 +0900

    [style] 박람회, 페스티벌 생성하는 코드 삭제 (#36)

--- src/main/java/hyangyu/server/api/FavoriteApi.java
+++ src/main/java/hyangyu/server/api/FavoriteApi.java
@@ -65,10 +65,6 @@ public class FavoriteApi {
         Long userId = 1L;
         HttpHeaders httpHeaders = new HttpHeaders();
 
-        EventDto eventDto = new EventDto(Date.valueOf("2021-01-24"), Date.valueOf("2021-01-28"), "테스트 박람회", 5, 0, Time.valueOf("09:00:00"), Time.valueOf("18:00:00"), Time.valueOf("09:00:00"), Time.valueOf("18:00:00"), "서울", "향유박람회", Date.valueOf("2021-01-26"), "세부내용", "", "", "", 0);
-        Fair fair1 = Fair.createFair(eventDto);
-        fairService.saveFair(fair1);
-
         //박람회 검색
         Optional<Fair> fair = fairService.findOne(fairId);
         if(fair.isEmpty()) {
@@ -96,9 +92,6 @@ public class FavoriteApi {
         Long userId = 1L;
         HttpHeaders httpHeaders = new HttpHeaders();
 
-        EventDto eventDto = new EventDto(Date.valueOf("2021-01-24"), Date.valueOf("2021-01-28"), "테스트 페스티벌", 5, 0, Time.valueOf("09:00:00"), Time.valueOf("18:00:00"), Time.valueOf("09:00:00"), Time.valueOf("18:00:00"), "서울", "향유페스티벌", Date.valueOf("2021-01-26"), "세부내용", "", "", "", 0);
-        Festival f = Festival.createFestival(eventDto);
-        festivalService.saveFestival(f);
 
         //박람회 검색
         Optional<Festival> festival = festivalService.findOne(festivalId);
