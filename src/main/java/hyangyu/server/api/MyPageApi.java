9b039cebc6dcef206a105bba5bc432c82fa8e276
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-02-06 23:53:53 +0900

    [feat] 마이페이지 조회 api 완성 (#36)

--- src/main/java/hyangyu/server/api/MyPageApi.java
+++ src/main/java/hyangyu/server/api/MyPageApi.java
@@ -2,6 +2,7 @@ package hyangyu.server.api;
 
 import hyangyu.server.domain.User;
 import hyangyu.server.dto.ErrorDto;
+import hyangyu.server.dto.UserDto;
 import hyangyu.server.dto.myPage.MyPageDto;
 import hyangyu.server.dto.myPage.MyPageResponseDto;
 import hyangyu.server.service.MyPageService;
@@ -26,16 +27,17 @@ public class MyPageApi {
 
     @GetMapping("/myPage")
     public ResponseEntity getMyPage() throws Exception {
-        Long userId = 1L;
         HttpHeaders httpHeaders = new HttpHeaders();
 
         //사용자 검색
-        Optional<User> user = userService.findUser(userId);
-        if(user.isEmpty()) {
+        UserDto user = userService.getMyUserWithAuthorities();
+        if(user == null) {
             return new ResponseEntity(new ErrorDto(401, "유효하지 않은 사용자입니다."), HttpStatus.BAD_REQUEST);
         }
 
-        MyPageDto myPageDto = myPageService.getMyPage();
+        MyPageDto myPageDto = myPageService.getMyPage(user.getUserId());
+        myPageDto.setImage(user.getImage());
+        myPageDto.setUsername(user.getUsername());
         MyPageResponseDto myPageResponseDto = new MyPageResponseDto(200, myPageDto);
         return new ResponseEntity<>(myPageResponseDto, httpHeaders, HttpStatus.OK);
     }
