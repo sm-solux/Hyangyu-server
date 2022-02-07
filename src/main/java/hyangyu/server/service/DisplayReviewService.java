68250d769bb39108ab9c9203b9a0107b5f866e8c
yujin113 <claire9585@sookmyung.ac.kr>
2022-02-07 17:28:27 +0900

    [feat] 리뷰 신고 service 로직 추가(#50)

--- src/main/java/hyangyu/server/service/DisplayReviewService.java
+++ src/main/java/hyangyu/server/service/DisplayReviewService.java
@@ -2,10 +2,12 @@ package hyangyu.server.service;
 
 import hyangyu.server.domain.Display;
 import hyangyu.server.domain.DisplayReview;
+import hyangyu.server.domain.DisplayWarn;
 import hyangyu.server.domain.User;
 import hyangyu.server.dto.RequestReviewDto;
 import hyangyu.server.repository.DisplayRepository;
 import hyangyu.server.repository.DisplayReviewRepository;
+import hyangyu.server.repository.DisplayWarnRepository;
 import hyangyu.server.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,15 +18,15 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
-@Transactional(readOnly = true)
+@Transactional
 @RequiredArgsConstructor
 public class DisplayReviewService {
 
     private final DisplayReviewRepository displayReviewRepository;
+    private final DisplayWarnRepository displayWarnRepository;
     private final UserRepository userRepository;
     private final DisplayRepository displayRepository;
 
-    @Transactional(readOnly = false)
     public Long saveDisplayReview(Long userId, Long displayId, RequestReviewDto requestReviewDto) {
         Optional<User> user = userRepository.findById(userId);
         Optional<Display> display = displayRepository.findOne(displayId);
@@ -38,17 +40,43 @@ public class DisplayReviewService {
         }
     }
 
-    @Transactional(readOnly = false)
     public Optional<DisplayReview> modifyDisplayReview(Long userId, Long displayId, RequestReviewDto requestReviewDto) {
         Optional<DisplayReview> displayReview = Optional.ofNullable(displayReviewRepository.getDisplayReview(displayId, userId));
         displayReview.ifPresent(review -> review.updateDisplayReview(requestReviewDto.getContent(), requestReviewDto.getScore()));
         return displayReview;
     }
 
-    @Transactional
     public Optional<DisplayReview> deleteDisplayReview(Long userId, Long displayId) {
         Optional<DisplayReview> displayReview = Optional.ofNullable(displayReviewRepository.getDisplayReview(displayId, userId));
         displayReview.ifPresent(displayReviewRepository::delete);
         return displayReview;
+    }
+
+    public Optional<DisplayReview> findReview(Long reviewId) {
+        return displayReviewRepository.findById(reviewId);
+    }
+
+    public String accuseDisplayReview(DisplayReview displayReview, Long userId) {
+        Optional<User> user = userRepository.findById(userId);
+        if (userId.equals(displayReview.getUser().getUserId())) {
+            return "내가 쓴 리뷰는 신고할 수 없습니다.";
+        }
+
+        Optional<DisplayWarn> displayWarn = Optional.ofNullable(displayWarnRepository.getDisplayWarn(displayReview.getReviewId(), userId));
+        if (displayWarn.isEmpty() && user.isPresent()) {
+            DisplayWarn warn = DisplayWarn.createDisplayWarn(displayReview, user.get());
+            displayWarnRepository.save(warn);
+
+            displayReview.increaseWarn();
+            if (displayReview.getWarn() == 3) {
+                displayWarnRepository.deleteDisplayWarns(displayReview.getReviewId());
+                displayReviewRepository.delete(displayReview);
+                return "신고 3번이 누적되어 자동 삭제되었습니다.";
+            }
+        } else if (displayWarn.isPresent()) {
+            return "이미 신고한 리뷰입니다.";
+        }
+
+        return "신고가 완료되었습니다.";
     }
 }
