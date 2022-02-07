7d7a08a087fc60b9fd5f47298ebd12b4a2894782
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-02-08 01:55:26 +0900

    [feat] 카테고리 가져오는 코드 추가 (#66)

--- src/main/java/hyangyu/server/repository/DisplayRepository.java
+++ src/main/java/hyangyu/server/repository/DisplayRepository.java
@@ -1,25 +1,29 @@
 package hyangyu.server.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import hyangyu.server.domain.Display;
-import hyangyu.server.domain.QDisplay;
-import hyangyu.server.domain.QFair;
-import hyangyu.server.domain.QFestival;
+import hyangyu.server.domain.*;
+import hyangyu.server.dto.EventDto;
 import hyangyu.server.dto.event.DisplayDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-@RequiredArgsConstructor
 public class DisplayRepository {
 
     private final EntityManager em;
     private final JPAQueryFactory queryFactory;
     QDisplay display = QDisplay.display;
+    QFavoriteDisplay favoriteDisplay = QFavoriteDisplay.favoriteDisplay;
+
+    public DisplayRepository(EntityManager em) {
+        this.em = em;
+        this.queryFactory = new JPAQueryFactory(em);
+    }
 
     public void saveDisplay(Display display) {
         em.persist(display);
@@ -30,31 +34,53 @@ public class DisplayRepository {
     }
 
     public DisplayDto getDisplay(Long userId, String order, int page) {
+
+        List<Display> displays = new ArrayList<>();
+
         if (order.equals("latest")) {
+            displays = queryFactory.select(display)
+                    .from(display)
+                    .orderBy(display.startDate.desc())
+                    .offset(page*10)
+                    .limit(10)
+                    .fetch();
 
         }
 
         else if (order.equals("popularity")) {
-
+            displays = queryFactory.select(display)
+                    .from(display)
+                    .orderBy(display.likey.desc())
+                    .offset(page*10)
+                    .limit(10)
+                    .fetch();
         }
 
         else if (order.equals("charge")) {
-
+            displays = queryFactory.select(display)
+                    .from(display)
+                    .where(display.price.gt(0))
+                    .offset(page*10)
+                    .limit(10)
+                    .fetch();
         }
 
         else if (order.equals("free")) {
-
+            displays = queryFactory.select(display)
+                    .from(display)
+                    .where(display.price.eq(0))
+                    .offset(page*10)
+                    .limit(10)
+                    .fetch();
         }
 
-        else {
-            //예외처리 해주세요~
+        List<EventDto> displayList = new ArrayList<>();
+        for (Display display : displays) {
+            EventDto eventDto = new EventDto(display);
+            eventDto.setSaved(false);
+            displayList.add(eventDto);
         }
 
-        List<Display> displays = queryFactory.select(display)
-                .from(display)
-                .orderBy(display.startDate.desc())
-                .offset(page*10)
-                .limit(10)
-                .fetch();
+        return new DisplayDto(displayList);
     }
 }
