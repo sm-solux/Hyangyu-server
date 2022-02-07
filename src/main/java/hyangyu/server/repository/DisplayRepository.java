02c1c6567d8deb58043e410ae801edf26213b70a
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-02-07 19:00:31 +0900

    [feat] 카테고리용 전시 repository (#66)

--- src/main/java/hyangyu/server/repository/DisplayRepository.java
+++ src/main/java/hyangyu/server/repository/DisplayRepository.java
@@ -1,10 +1,16 @@
 package hyangyu.server.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import hyangyu.server.domain.Display;
+import hyangyu.server.domain.QDisplay;
+import hyangyu.server.domain.QFair;
+import hyangyu.server.domain.QFestival;
+import hyangyu.server.dto.event.DisplayDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,6 +18,8 @@ import java.util.Optional;
 public class DisplayRepository {
 
     private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
+    QDisplay display = QDisplay.display;
 
     public void saveDisplay(Display display) {
         em.persist(display);
@@ -19,5 +27,34 @@ public class DisplayRepository {
 
     public Optional<Display> findOne(Long displayId) {
         return Optional.ofNullable(em.find(Display.class, displayId));
+    }
+
+    public DisplayDto getDisplay(Long userId, String order, int page) {
+        if (order.equals("latest")) {
+
+        }
+
+        else if (order.equals("popularity")) {
+
+        }
+
+        else if (order.equals("charge")) {
+
+        }
+
+        else if (order.equals("free")) {
+
+        }
+
+        else {
+            //예외처리 해주세요~
+        }
+
+        List<Display> displays = queryFactory.select(display)
+                .from(display)
+                .orderBy(display.startDate.desc())
+                .offset(page*10)
+                .limit(10)
+                .fetch();
     }
 }
