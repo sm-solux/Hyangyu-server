5591a39bcfdc258ee5ca54830682742a53de5a5c
yujin113 <claire9585@sookmyung.ac.kr>
2022-02-09 01:18:47 +0900

    [feat] 리뷰 조회 Dto 추가(#68)

--- src/main/java/hyangyu/server/dto/ReviewDto.java
+++ src/main/java/hyangyu/server/dto/ReviewDto.java
@@ -1,13 +1,23 @@
 package hyangyu.server.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 public class ReviewDto {
+
     String photo;
+
     String username;
-    int score;
+
+    @JsonFormat(pattern = "yyyy/MM/dd HH:mm", timezone = "Asia/Seoul")
+    LocalDateTime createTime;
+
     String content;
+
+    int score;
 }
